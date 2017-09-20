376ca0d8626c68c4b27d80a0885f166a1859df9e
sivan57846775 <32030157+sivan57846775@users.noreply.github.com>
2017-09-20 22:28:16 +0800

    Add files via upload

---
+++ exer-1-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+void mian()
+{
+	int n,i;
+	while(n!=0)
+		n=n/10;
+		i++;
+		switch(i)
+	{
+		case 0:cout<<"该数小于10"\n";break;
+		case 1:cout<<"该数小于10"\n";break;
+		case 2:cout<<"该数大于10小于100"\n";break;
+		case 3:cout<<"该数大于100小于1000"\n;break;
+		case 4:cout<<"该数大于1000小于10000"\n;break;
+defaullt:cout<<"请输入整数"\n";
+			 }
+}
